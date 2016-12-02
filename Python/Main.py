2c7e457b0c2d8dd709fa5f1dadb1b9693a4a8371
Fran Acien <amil101ftw@gmail.com>
2016-12-02 01:59:13 +0100

    adding some code, not tested

--- Python/Main.py
+++ Python/Main.py
@@ -1,10 +1,15 @@
 import TwitterClass
 import TWITTERDATA
 import time
+import os
+import random
 
 '''The api sends a tweet every 12 hours with a picture taken from a folder with random pics'''
 twitterObj = TwitterClass.TwitterClass(TWITTERDATA.CONSUMER_KEY, TWITTERDATA.CONSUMER_SECRET, TWITTERDATA.ACCESS_TOKEN, TWITTERDATA.ACCESS_TOKEN_SECRET)        #Ther twitter object of th etwitter class
 
+dirlist = os.listdir("../Pics/")             #Then save the paths of the pics in a aarray
+
 while True:
-    twitterObj.writeTweetWithMedia("etica.png", "Prueba con imagen")
+    random.seed()
+    twitterObj.writeTweetWithMedia(dirlist[random.randrange(0, len(dirlist))], "")
     time.sleep(43200)
